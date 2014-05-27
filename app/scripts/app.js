41749dd43c067df1a1b8cc7673f374cc3a8696b8
christopher.lloyd <christopher.lloyd@six3systems.com>
2014-05-27 15:09:06 -0400

    Added avatar upload

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,7 +4,8 @@ angular.module('postOfficeProjectApp', [
   'ngCookies',
   'ngResource',
   'ngSanitize',
-  'ngRoute'
+  'ngRoute',
+  'angularFileUpload'
 ])
   .config(function ($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider
--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -1,33 +1,68 @@
 'use strict';
 
 angular.module('postOfficeProjectApp')
-  .controller('SignupCtrl', function ($scope, Auth, $location) {
+  .controller('SignupCtrl', function ($scope, Auth, $location, $fileUploader) {
     $scope.user = {};
     $scope.errors = {};
 
+    var uploader = $scope.uploader = $fileUploader.create({
+      scope: $scope,
+      url: "api/users",
+      formData: [
+        { data: 'value' }
+      ]
+    });
+
+    uploader.bind('success', function(event, xhr, item, response) {
+      console.log(response.avatarId);
+      $location.path('/');
+    });
+
     $scope.register = function(form) {
       $scope.submitted = true;
-  
+
       if(form.$valid) {
-        Auth.createUser({
+        $scope.uploader.queue[$scope.uploader.queue.length - 1].formData = [{
           name: $scope.user.name,
           email: $scope.user.email,
           password: $scope.user.password
-        })
-        .then( function() {
-          // Account created, redirect to home
-          $location.path('/');
-        })
-        .catch( function(err) {
-          err = err.data;
-          $scope.errors = {};
+        }];
+        $scope.uploader.queue[$scope.uploader.queue.length - 1].upload();
+        // .then( function() {
+        //   // Account created, redirect to home
+        //   $location.path('/');
+        // })
+        // .catch( function(err) {
+        //   err = err.data;
+        //   $scope.errors = {};
 
-          // Update validity of form fields that match the mongoose errors
-          angular.forEach(err.errors, function(error, field) {
-            form[field].$setValidity('mongoose', false);
-            $scope.errors[field] = error.message;
-          });
-        });
+        //   // Update validity of form fields that match the mongoose errors
+        //   angular.forEach(err.errors, function(error, field) {
+        //     form[field].$setValidity('mongoose', false);
+        //     $scope.errors[field] = error.message;
+        //   });
+        // });
+
+      // if(form.$valid) {
+      //   Auth.createUser({
+      //     name: $scope.user.name,
+      //     email: $scope.user.email,
+      //     password: $scope.user.password
+      //   })
+      //   .then( function() {
+      //     // Account created, redirect to home
+      //     $location.path('/');
+      //   })
+      //   .catch( function(err) {
+      //     err = err.data;
+      //     $scope.errors = {};
+
+      //     // Update validity of form fields that match the mongoose errors
+      //     angular.forEach(err.errors, function(error, field) {
+      //       form[field].$setValidity('mongoose', false);
+      //       $scope.errors[field] = error.message;
+      //     });
+      //   });
       }
     };
   });
