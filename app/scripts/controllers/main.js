e757a6caac26089315deafb138a5e794c21c66fd
christopher.lloyd <christopher.lloyd@six3systems.com>
2014-05-30 11:27:00 -0400

    stuff

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,7 +1,7 @@
 'use strict';
 
 angular.module('postOfficeProjectApp')
-  .controller('MainCtrl', function ($scope, Auth) {
+  .controller('MainCtrl', function ($scope, Auth, $location) {
   	$scope.logout = function() {
       Auth.logout()
       .then(function() {
