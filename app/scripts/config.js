6a2e49292ee01ba36b32d20f82c6099ccb1df959
christopher.lloyd <christopher.lloyd@six3systems.com>
2014-06-04 15:55:03 -0400

    adjusted second template

--- app/scripts/config.js
+++ app/scripts/config.js
@@ -34,16 +34,29 @@ angular.module('postOfficeProjectApp')
   		src: "images/orange.jpeg",
   		address: {
   			position: "absolute",
-  			top: "0",
-  			left: "500px",
+  			top: "280px",
+  			left: "10px",
   			color: "black"
   		},
   		image: {
   			position: "absolute",
-  			top: "0",
-  			left: "500px",
-  			width: "80px",
-  			height: "80px"	
+  			top: "300px",
+  			left: "10px",
+  			width: "300px",
+  			height: "200px"	
+  		},
+  		agentPic: {
+  			position: "absolute",
+  			top: "0px",
+  			left: "560px",
+  			width: "150px",
+  			height: "200px"		
+  		},
+  		agentName: {
+  			position: "absolute",
+  			top: "210px",
+  			left: "600px",
+  			color: "black"	
   		}
   	}]
   });
